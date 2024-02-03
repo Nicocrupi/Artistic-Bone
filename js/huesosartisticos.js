6711c22f2f31fce4518503479710aadd512ad71f
Nicolas Cruz Pineda <nicolascruzpineda@hotmail.com>
2024-02-02 22:59:34 -0500

    Firts Commit

---
+++ js/huesosartisticos.js
@@ -0,0 +1,73 @@
+const buttonAmarillo = document.getElementById("button-amarillo")
+const buttonRojo = document.getElementById("button-rojo")
+const buttonAzul = document.getElementById("button-azul")
+const buttonVerde = document.getElementById("button-verde")
+const buttonInicio = document.getElementById("button-inicio")
+
+
+const sectionAmarillo = document.getElementById("section-amarillo")
+const sectionRojo = document.getElementById("section-rojo")
+const sectionAzul = document.getElementById("section-azul")
+const sectionVerde = document.getElementById("section-verde")
+const sectionPrincipal = document.getElementById("section-principal")
+const sectionInitial = document.getElementById("section-initial")
+
+buttonAmarillo.addEventListener("click", activeSectionAmarillo)
+buttonRojo.addEventListener("click", activeSectionRojo)
+buttonAzul.addEventListener("click", activeSectionArtistas)
+buttonVerde.addEventListener("click", activeSectionProyectos)
+buttonInicio.addEventListener("click", activeSectionInicio)
+
+function iniciarPage(){
+    sectionAmarillo.style.display = "none"
+    sectionRojo.style.display = "none"
+    sectionAzul.style.display = "none"
+    sectionVerde.style.display = "none"
+    sectionPrincipal.style.display = "block"
+    console.log("Page Lista")
+    
+}
+function activeSectionInicio(){
+    sectionInitial.style.display = "block"
+    sectionAmarillo.style.display = "none"
+    sectionRojo.style.display = "none"
+    sectionAzul.style.display = "none"
+    sectionVerde.style.display = "none"
+    console.log("Todo ok en Inicio")
+}
+function activeSectionAmarillo(){
+    sectionInitial.style.display = "none"
+    sectionRojo.style.display = "none"
+    sectionAzul.style.display = "none"
+    sectionVerde.style.display = "none"
+    sectionAmarillo.style.display = "block"
+    console.log("Todo ok en Home")
+}
+
+function activeSectionRojo(){
+    sectionInitial.style.display = "none"
+    sectionAmarillo.style.display = "none"
+    sectionRojo.style.display = "block"
+    sectionAzul.style.display = "none"
+    sectionVerde.style.display = "none"
+    console.log("Todo ok en Nosotros")
+}
+
+function activeSectionArtistas(){
+    sectionInitial.style.display = "none"
+    sectionAmarillo.style.display = "none"
+    sectionRojo.style.display = "none"
+    sectionAzul.style.display = "block"
+    sectionVerde.style.display = "none"
+    console.log("Todo ok en Artistas")
+}
+
+function activeSectionProyectos(){
+    sectionInitial.style.display = "none"
+    sectionAmarillo.style.display = "none"
+    sectionRojo.style.display = "none"
+    sectionAzul.style.display = "none"
+    sectionVerde.style.display = "block"
+    console.log("Todo ok en Proyectos")
+}
+window.addEventListener("load", iniciarPage)
