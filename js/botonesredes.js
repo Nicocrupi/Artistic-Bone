1425a8f908f3a59931ffd2c7b321ba655ad0bf6d
Nicolas Cruz Pineda <nicolascruzpineda@hotmail.com>
2024-03-01 00:50:07 -0500

    Actualizacion de botones y links de redes sociales

--- js/botonesredes.js
+++ js/botonesredes.js
@@ -7,6 +7,9 @@ const botonFOn = document.getElementById("boton-fOn")
 const botonFBack = document.getElementById("boton-fBack")
 const botonMOn = document.getElementById("boton-mOn")
 const botonMBack = document.getElementById("boton-mBack")
+const botonEOn = document.getElementById("boton-eOn")
+const botonEBack = document.getElementById("boton-eBack")
+
 
 botonWOn.addEventListener("click", botonWON)
 botonWBack.addEventListener("click", botonWOFF)
@@ -16,6 +19,8 @@ botonFOn.addEventListener("click", botonFON)
 botonFBack.addEventListener("click", botonFOFF)
 botonMOn.addEventListener("click", botonMON)
 botonMBack.addEventListener("click", botonMOFF)
+botonEOn.addEventListener("click", botonEON)
+botonEBack.addEventListener("click", botonEOFF)
 
 
 const iconOnW = document.getElementById("icon-On-W")
@@ -26,17 +31,41 @@ const iconOnF = document.getElementById("icon-On-F")
 const iconOffF = document.getElementById("icon-Off-F")
 const iconOnM = document.getElementById("icon-On-M")
 const iconOffM = document.getElementById("icon-Off-M")
+const iconOnE = document.getElementById("icon-On-E")
+const iconOffE = document.getElementById("icon-Off-E")
 const pjSkelW = document.getElementById("pjSkeletonW")
 const pjSkelF = document.getElementById("pjSkeletonF")
+const pjSkelE = document.getElementById("pjSkeletonE")
 const pjSkelY = document.getElementById("pjSkeletonY")
 const pjSkelM = document.getElementById("pjSkeletonM")
 const pjSkelCell = document.getElementById("pjSkeletonCell")
 
+const linkY = document.getElementById("link-Y")
+const linkW = document.getElementById("link-W")
+const linkE = document.getElementById("link-E")
+const linkF = document.getElementById("link-F")
+
 function botonWON(){
+    linkY.style.display = "none"
+    linkW.style.display = "block"
+    linkE.style.display = "none"
+    linkF.style.display = "none"
+    botonYBack.style.display = "none"
+    botonEBack.style.display = "none"
+    botonMBack.style.display = "none"
+    botonFBack.style.display = "none"
+    botonWBack.style.display = "block"
+    botonWOn.style.display = "none"
+    botonEOn.style.display = "block"
+    botonYOn.style.display = "block"
+    botonFOn.style.display = "block"
+    botonMOn.style.display = "block"
     pjSkelCell.style.display = "none"
     pjSkelM.style.display = "none"
     pjSkelF.style.display = "none"
     pjSkelY.style.display = "none"
+    pjSkelE.style.display = "none"
+    iconOnE.style.display = "none"
     iconOnY.style.display = "none"
     iconOnM.style.display = "none"
     iconOnF.style.display = "none"
@@ -44,55 +73,134 @@ function botonWON(){
     iconOffY.style.display = "block"
     iconOffM.style.display = "block"
     iconOffF.style.display = "block"
-    iconOffW.style.display = "none"
+    iconOffE.style.display = "block"
     iconOnW.style.display = "block"
     pjSkelW.style.display = "block"
     console.log("botonWOn Funcionando")
 }
 function botonWOFF(){
+    linkW.style.display = "none"
+    botonWOn.style.display = "block"
+    botonWBack.style.display = "none"
     pjSkelCell.style.display = "block"
-    pjSkelM.style.display = "none"
-    pjSkelF.style.display = "none"
-    pjSkelY.style.display = "none"
-    iconOnW.style.display = "none"
     pjSkelW.style.display = "none"
+    iconOnW.style.display = "none"
     iconOffW.style.display = "block"
     console.log("botonWOff Funcionando")
 }
 function botonYON(){
+    linkY.style.display = "block"
+    linkW.style.display = "none"
+    linkE.style.display = "none"
+    linkF.style.display = "none"
+    botonEBack.style.display = "none"
+    botonWBack.style.display = "none"
+    botonMBack.style.display = "none"
+    botonFBack.style.display = "none"
+    botonYBack.style.display = "block"
+    botonWOn.style.display = "block"
+    botonEOn.style.display = "block"
+    botonYOn.style.display = "none"
+    botonFOn.style.display = "block"
+    botonMOn.style.display = "block"
     pjSkelCell.style.display = "none"
     pjSkelM.style.display = "none"
     pjSkelF.style.display = "none"
     pjSkelW.style.display = "none"
+    pjSkelE.style.display = "none"
+    iconOnE.style.display = "none"
     iconOnW.style.display = "none"
     iconOnM.style.display = "none"
     iconOnF.style.display = "none"
+    iconOffY.style.display = "none"
     iconOffW.style.display = "block"
     iconOffM.style.display = "block"
     iconOffF.style.display = "block"
-    iconOffY.style.display = "none"
+    iconOffE.style.display = "block"
     iconOnY.style.display = "block"
     pjSkelY.style.display = "block"
-    console.log("botonWOn Funcionando")
+    console.log("botonYOn Funcionando")
 }
 function botonYOFF(){
+    linkY.style.display = "none"
+    botonYOn.style.display = "block"
+    botonYBack.style.display = "none"
     pjSkelCell.style.display = "block"
+    pjSkelY.style.display = "none"
+    iconOnY.style.display = "none"
+    iconOffY.style.display = "block"
+    console.log("botonYOff Funcionando")
+}
+function botonEON(){
+    linkY.style.display = "none"
+    linkW.style.display = "none"
+    linkE.style.display = "block"
+    linkF.style.display = "none"
+    botonYBack.style.display = "none"
+    botonWBack.style.display = "none"
+    botonMBack.style.display = "none"
+    botonFBack.style.display = "none"
+    botonEBack.style.display = "block"
+    botonWOn.style.display = "block"
+    botonEOn.style.display = "none"
+    botonYOn.style.display = "block"
+    botonFOn.style.display = "block"
+    botonMOn.style.display = "block"
+    pjSkelCell.style.display = "none"
     pjSkelM.style.display = "none"
     pjSkelF.style.display = "none"
     pjSkelY.style.display = "none"
     pjSkelW.style.display = "none"
     iconOnY.style.display = "none"
+    iconOnM.style.display = "none"
+    iconOnF.style.display = "none"
+    iconOnW.style.display = "none"
+    iconOffE.style.display = "none"
+    iconOffW.style.display = "none"
+    iconOnE.style.display = "block"
+    iconOffW.style.display = "block"
     iconOffY.style.display = "block"
-    console.log("botonWOff Funcionando")
+    iconOffM.style.display = "block"
+    iconOffF.style.display = "block"
+    pjSkelE.style.display = "block"
+    console.log("botonEOn Funcionando")
+}
+function botonEOFF(){
+    linkE.style.display = "none"
+    botonEOn.style.display = "block"
+    botonEBack.style.display = "none"
+    pjSkelCell.style.display = "block"
+    pjSkelE.style.display = "none"
+    iconOnW.style.display = "none"
+    iconOffE.style.display = "block"
+    iconOnE.style.display = "none"
+    console.log("botonEOff Funcionando")
 }
 function botonFON(){
+    linkY.style.display = "none"
+    linkW.style.display = "none"
+    linkE.style.display = "none"
+    linkF.style.display = "block"
+    botonYBack.style.display = "none"
+    botonWBack.style.display = "none"
+    botonMBack.style.display = "none"
+    botonEBack.style.display = "none"
+    botonFBack.style.display = "block"
+    botonWOn.style.display = "block"
+    botonEOn.style.display = "block"
+    botonYOn.style.display = "block"
+    botonFOn.style.display = "none"
+    botonMOn.style.display = "block"
     pjSkelCell.style.display = "none"
     pjSkelM.style.display = "none"
-    pjSkelY.style.display = "none"
     pjSkelW.style.display = "none"
+    pjSkelY.style.display = "none"
+    pjSkelE.style.display = "none"
+    iconOnE.style.display = "none"
     iconOnY.style.display = "none"
     iconOnM.style.display = "none"
     iconOnW.style.display = "none"
+    iconOffE.style.display = "block"
     iconOffY.style.display = "block"
     iconOffM.style.display = "block"
     iconOffW.style.display = "block"
@@ -102,23 +210,36 @@ function botonFON(){
     console.log("botonWOn Funcionando")
 }
 function botonFOFF(){
+    linkF.style.display = "none"
+    botonFOn.style.display = "block"
+    botonFBack.style.display = "none"
     pjSkelCell.style.display = "block"
-    pjSkelM.style.display = "none"
     pjSkelF.style.display = "none"
-    pjSkelY.style.display = "none"
-    pjSkelW.style.display = "none"
     iconOnF.style.display = "none"
     iconOffF.style.display = "block"
     console.log("botonWOff Funcionando")
 }
 function botonMON(){
+    botonYBack.style.display = "none"
+    botonWBack.style.display = "none"
+    botonEBack.style.display = "none"
+    botonFBack.style.display = "none"
+    botonMBack.style.display = "block"
+    botonWOn.style.display = "block"
+    botonEOn.style.display = "block"
+    botonYOn.style.display = "block"
+    botonFOn.style.display = "block"
+    botonMOn.style.display = "none"
     pjSkelCell.style.display = "none"
+    pjSkelW.style.display = "none"
     pjSkelF.style.display = "none"
     pjSkelY.style.display = "none"
-    pjSkelW.style.display = "none"
+    pjSkelE.style.display = "none"
+    iconOnE.style.display = "none"
     iconOnY.style.display = "none"
     iconOnW.style.display = "none"
     iconOnF.style.display = "none"
+    iconOffE.style.display = "block"
     iconOffY.style.display = "block"
     iconOffW.style.display = "block"
     iconOffF.style.display = "block"
@@ -128,11 +249,10 @@ function botonMON(){
     console.log("botonWOn Funcionando")
 }
 function botonMOFF(){
+    botonMOn.style.display = "block"
+    botonMBack.style.display = "none"
     pjSkelCell.style.display = "block"
     pjSkelM.style.display = "none"
-    pjSkelF.style.display = "none"
-    pjSkelY.style.display = "none"
-    pjSkelW.style.display = "none"
     iconOnM.style.display = "none"
     iconOffM.style.display = "block"
     console.log("botonWOff Funcionando")
